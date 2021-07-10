eb996c51f58bfae8ebc0042042e0afa792a23954
ghigbie <gehigbie@gmail.com>
2021-07-10 11:13:10 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const request = require('request-promise');
+const cheerio = require('cheerio');
+
+const movieTitleURL = 'https://www.imdb.com/chart/top/?ref_=nv_mv_250';
+
+const scrapeTitleList = async (scrapeTargetURL = movieTitleURL) => {
+    const result = await request(scrapeTargetURL);
+    const $ = cheerio.load(result);
+    const movies = $('tr').map( (index, element) => {
+        return {
+            title: $(element).find('td.titleColumn > a').text(),
+            url: $(element).find('td.titleColumn > a').attr('href'),
+            year: $(element).find('td.titleColumn > span.secondaryInfo').text(),
+            rating: $(element).find('td.ratingColumn.imdbRating > strong').text(),
+        };
+    }).get();
+    console.log(movies);
+}
+
+scrapeTitleList();
